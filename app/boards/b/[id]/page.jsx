c6659168dc38f321237762a02b5f4da26098f9b6
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-06-23 18:29:29 +0530

    [Board.jsx] list add button added

--- app/boards/b/[id]/page.jsx
+++ app/boards/b/[id]/page.jsx
@@ -3,12 +3,16 @@ import "@/style/board.css"
 import { useSession } from 'next-auth/react'
 import { redirect, useSearchParams } from 'next/navigation'
 import BoardNav from '@/components/board_components/BoardNav'
+import { useState } from "react"
+import { BsXLg } from "react-icons/bs"
 
 const Board = ({params}) => {
   
   const searchParams = useSearchParams()  
   const boardName = searchParams.get("title")
-  console.log("board Name: ",boardName);
+
+  const [addList,setAddList] = useState(false)
+
   const {data:session} = useSession({
     required: true,
         onUnauthenticated() {
@@ -16,21 +20,47 @@ const Board = ({params}) => {
         }
   })  
 
+  const handleListFormSubmit = () =>{
+
+  }
+
   return (
     <section className='w-full'>
       <BoardNav boardName={boardName}/>
       <div className='list_container h-[84vh]'>
-        <div className="addList_btn flex_center">Add List</div>
-        <div className="addList_btn flex_center">Add List</div>
-        <div className="addList_btn flex_center">Add List</div>
-        <div className="addList_btn flex_center">Add List</div>
-        <div className="addList_btn flex_center">Add List</div>
-        <div className="addList_btn flex_center">Add List</div>
-        <div className="addList_btn flex_center">Add List</div>
-        <div className="addList_btn flex_center">Add List</div>
-        <div className="addList_btn flex_center">Add List</div>
-        <div className="addList_btn flex_center">Add List</div>
-        <div className="addList_btn flex_center">Add List</div>
+        {!addList ? (
+          <button 
+            onClick={()=>setAddList(true)}
+            className="addList_btn flex_center"
+          >
+            Add List
+          </button>
+        ) : (
+          <form 
+            onSubmit={handleListFormSubmit}
+            className="addList_form flex flex-col gap-2"
+          >
+            <input 
+              type="text" 
+              placeholder="Enter List Title..."
+              className="addList_formInput"
+            />
+            <div>
+            <span className="flex items-center gap-2">
+              <button 
+                type="submit"
+                className="addList_formSubmit_btn" 
+                >  
+                Add 
+              </button>
+              <BsXLg 
+                onClick={()=>setAddList(false)}
+                className="cursor-pointer"
+              />
+            </span>  
+          </div>
+          </form> 
+        )}
       </div>
     </section>
   )
