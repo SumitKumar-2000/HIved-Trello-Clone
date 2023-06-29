b5e0eb5c3d84cca2ede0385f57c7f73822a6e33d
SumitKumar-2000 <sumitkumarracer@gmail.com>
2023-06-30 03:26:26 +0530

    [TaskList.jsx] card image functionality added

--- components/board_components/taskList_components/TaskList.jsx
+++ components/board_components/taskList_components/TaskList.jsx
@@ -1,85 +1,110 @@
-"use client"
-import "@/style/board.css"
-import { useState } from "react"
-import { BsThreeDotsVertical, BsPlusLg, BsXLg } from "react-icons/bs"
+"use client";
+import "@/style/board.css";
+import Image from "next/image";
+import { useState } from "react";
+import { BsThreeDotsVertical, BsPlusLg, BsXLg, BsCardImage } from "react-icons/bs";
 
-const TaskList = ({taskList}) => {
-
-  const [cardAdd, setCardAdd] =  useState(false)
-  const [loading, setLoading] = useState(false)
+const TaskList = ({ taskList }) => {
+  const [cardAdd, setCardAdd] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [cardFormData, setCardFormData] = useState({
-    title: "",
+    title:"",
     description: "",
-    image: "",
-  })
+    image: null,
+  });
 
   const handleCardAdd = (e) => {
     e.preventDefault();
-    console.log("form data: ",cardFormData); 
+    setLoading(true)
+    console.log("card form data: ",cardFormData);
 
-    setCardAdd(false)
-  }
+    setCardFormData({
+      title: "",
+      description: "",
+      image: null
+    })
+    setLoading(false)
+    setCardAdd(false);
+  };
 
   return (
     <section className="taskList flex_center flex-col scrollbar-none">
       <div className="w-full flex_between mb-4 pl-2">
         <span className="font-semibold">{taskList.title}</span>
         <button className="p-2 flex_center hover:bg-zinc-800 dark:hover:bg-white rounded-sm">
-          <BsThreeDotsVertical className="cursor-pointer"/>
+          <BsThreeDotsVertical className="cursor-pointer" />
         </button>
       </div>
       {cardAdd ? (
-        <form 
+        <form
           onSubmit={handleCardAdd}
           className="addList_form flex flex-col gap-2"
         >
-          <input 
+          <input
             required
-            type="text" 
+            type="text"
+            name="title"
             placeholder="Add title *"
             className="addList_formInput"
             value={cardFormData.title}
-            onChange={(e)=>setCardFormData({...cardFormData, title: e.target.value})}
+            onChange={(e) => setCardFormData({...cardFormData,title:e.target.value})}
             />
-          <textarea 
-            type="text" 
+          <textarea
+            type="text"
             maxLength="60"
+            name="description"
             placeholder="Add description"
             className="addList_formInput"
             value={cardFormData.description}
-            onChange={(e)=>setCardFormData({...cardFormData, description: e.target.value})}
-          />
-          <input 
-            type="text" 
-            placeholder="Add image"
-            className="addList_formInput"
-            value={cardFormData.image}
-            onChange={(e)=>setCardFormData({...cardFormData, image: e.target.value})}
-          />
-          <span className="flex items-center gap-2">
-              <button 
-                type="submit"
-                className="addList_formSubmit_btn" 
-              >  
-                {loading ? "Adding..." : "Add"} 
-              </button>
-              <BsXLg 
-                onClick={()=>setCardAdd(false)}
-                className="cursor-pointer"
+            onChange={(e) => setCardFormData({...cardFormData,description:e.target.value})}
+            />
+          <div
+            className="addImage_btn"
+            onClick={()=>document.getElementById("selectImage").click()}
+          >
+            <input
+              hidden
+              type="file"
+              name="image"
+              accept="image/*"
+              id="selectImage"
+              onChange={(e) => setCardFormData({...cardFormData,image:URL.createObjectURL(e.target.files[0])})}
+            />
+            {cardFormData.image === null ? (
+              <BsCardImage
+                className="h-[16px] w-[16px]"
               />
-            </span>
+            ) : (
+              <Image
+                src={cardFormData.image}
+                alt="uploaded image"
+                width={16}
+                height={16}
+              />
+            )}
+            <span>{cardFormData.image === null ? "Add image" : "Add image again"}</span>
+          </div>
+          <span className="flex items-center gap-2">
+            <button type="submit" className="addList_formSubmit_btn">
+              {loading ? "Adding..." : "Add"}
+            </button>
+            <BsXLg
+              onClick={() => setCardAdd(false)}
+              className="cursor-pointer"
+            />
+          </span>
         </form>
       ) : (
-        <button 
+        <button
           className="flex_left addCard_btn"
-          onClick={()=>setCardAdd(true)}
+          onClick={() => setCardAdd(true)}
         >
           <BsPlusLg className="text-white dark:text-black" />
           <span>Add a card</span>
         </button>
       )}
     </section>
-  )
-}
+  );
+};
 
-export default TaskList
+export default TaskList;
